73634c946a7dd8aed9715159f6e02582b27222dc
Muhammad Idrees Lang <147036880+Malikidrees7613@users.noreply.github.com>
2024-09-04 13:45:54 +0000

    adding code in app file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit as st
-from 
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import joblib
+
+model= joblib.load('K-Nearest Neighbors.pkl',)
+
+with open
