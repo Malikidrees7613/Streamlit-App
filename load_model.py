291345dbcc7132365c7d4f41634f920238e1e3d4
Muhammad Idrees Lang <147036880+Malikidrees7613@users.noreply.github.com>
2024-09-04 13:32:27 +0000

    adding application code for streamlit

---
+++ load_model.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import joblib
+from sklearn.metrics import accuracy_score
+
+model=joblib.load("K-Nearest Neighbors.pkl")
+
+test_data= pd.read_csv("mobile_price_range_data.csv")
+
+x_test= test_data.iloc[:,:-1]
+y_test= test_data.iloc[:,-1]
+
+y_pred = model.predict(x_test)
+
+accuracy= accuracy_score(y_test,y_pred)
+
+with open('accuracy.txt','w') as file:
+    file.write(f'Accuracy:{accuracy}') 
